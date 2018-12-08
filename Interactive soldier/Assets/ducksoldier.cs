fdad3afe977dbfd029ff361d3000083422b6726c
Raja Ghosh <30506785+Raja2591@users.noreply.github.com>
2018-12-08 02:15:09 -0500

    Add project

---
+++ Interactive soldier/Assets/ducksoldier.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ducksoldier : MonoBehaviour
+{
+
+    Animator anim;
+
+    // Use this for initialization
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+
+    }
+
+    public void theduck()
+    {
+
+        anim.SetTrigger("Duck");
+    }
+
+    public void knockout()
+    {
+
+        anim.SetTrigger("Damage");
+    }
+
+    public void handsup()
+    {
+
+        anim.SetTrigger("Shoot");
+    }
+
+    public void rest()
+    {
+
+        anim.SetTrigger("Idle");
+    }
+}
